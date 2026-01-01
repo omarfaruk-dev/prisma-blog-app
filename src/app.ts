21eaf90934b7a2ce2cd57b725445e04394f3fdc5
Omar Faruk <omarfaruk.dev@gmail.com>
2026-01-01 13:20:56 +0600

    added: better auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import { postRouter } from './modules/post/post.router';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
+
 
 const app: Application = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send('Server is up and running!');
