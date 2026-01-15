9bac6cf29a1a3cff863011d86299936fbeccc3e4
omarfaruk-dev <omarfaruk.dev@gmail.com>
2026-01-15 11:42:12 +0600

    auth middleware created

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -3,8 +3,13 @@ import { PostService } from "./post.service"
 
 const createPost = async (req: Request, res: Response) => {
     try {
-        console.log(req.user)
-        const result = await PostService.createPost(req.body)
+        const user = req.user;
+        if(!user){
+            return res.status(400).json({
+                error: "Unauthorized User"
+            })
+        }
+        const result = await PostService.createPost(req.body, user.id as string)
         res.status(200).json(result)
     } catch (error) {
         res.status(500).json(error instanceof Error ? { message: error.message } : error)
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,66 +1,9 @@
-import express, { NextFunction, Request, Response, Router } from "express";
+import express, { Router } from "express";
 import { PostController } from "./post.controller";
-import { auth as betterAuth } from "../../lib/auth";
+import auth, { UserRole } from "../../middlewares/auth";
+
 
 const router = express.Router();
-
-export enum UserRole{
-    ADMIN = "ADMIN",
-    USER = "USER"
-}
-
-declare global{
-    namespace Express{
-        interface Request{
-            user?: {
-                id: string;
-                name: string;
-                email: string;
-                role: string;
-                emailVerified: boolean;
-            }
-        }
-    }
-}
-
-const auth = (...roles: UserRole[]) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        //get user session
-        const session = await betterAuth.api.getSession({
-            headers: req.headers as any
-
-        })
-          if(!session){
-                return res.status(401).json({
-                    success: false,
-                    message: "You are not authorized"
-                })
-            }
-            // if(!session.user.emailVerified){
-            //     return res.status(403).json({
-            //         success: false,
-            //         message: "Please verify your email to proceed"
-            //     }) 
-            // }
-            req.user = {
-                id: session.user.id,
-                name: session.user.name,
-                email: session.user.email,
-                role: session.user.role as string,
-                emailVerified: session.user.emailVerified
-            }
-
-            if(roles.length && !roles.includes(req.user.role as UserRole)){
-                return res.status(403).json({
-                    success: false,
-                    message: "You are not authorized to perform this action"
-                }) 
-            }
-
-            next();
-
-    }
-}
 
 router.post('/',
     auth(UserRole.USER),
