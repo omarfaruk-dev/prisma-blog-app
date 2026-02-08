43d477e6c65b7475a6ccb989f64690331c866dce
omarfaruk-dev <omarfaruk.dev@gmail.com>
2026-02-08 13:17:08 +0600

    comment created done

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { postRouter } from './modules/post/post.router';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from './lib/auth';
 import cors from 'cors';
+import { commentRouter } from './modules/comment/comment.router';
 
 const app: Application = express();
 
@@ -19,5 +20,6 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use(express.json());
 app.use('/posts', postRouter);
+app.use('/comments', commentRouter);
 
 export default app;
---
+++ src/modules/comment/comment.service.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../../lib/prisma";
+
+
+const createComment = async (payload: {
+    content: string;
+    postId: string;
+    authorId: string;
+    parentId?: string;
+}) => {
+
+    await prisma.post.findUniqueOrThrow({
+        where: {
+            id: payload.postId
+        }
+    })
+    if (payload.parentId) {
+        await prisma.comment.findUniqueOrThrow({
+            where: {
+                id: payload.parentId
+            }
+        })
+
+    }
+    return await prisma.comment.create({
+        data: payload
+    })
+}
+
+
+
+export const CommentService = {
+    createComment,
+}
